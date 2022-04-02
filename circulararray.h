c163285fb9c565733f7206e3d8ac42750571b31a
christopherwilliamflores <christopher.flores.ri@gmail.com>
2022-04-01 21:09:21 -0500

    change2

--- circulararray.h
+++ circulararray.h
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-template <class T>
+template <class T> //es plantilla XDD
 class CircularArray
 {
 private:
@@ -10,8 +10,8 @@ private:
     int back, front;
     
 public:
-    CircularArray();
-    CircularArray(int _capacity);
+    CircularArray(); //check
+    CircularArray(int _capacity); //check
     virtual ~CircularArray();
     void push_front(T data);
     void push_back(T data);
@@ -22,7 +22,7 @@ public:
     bool is_empty();
     int size();
     void clear();
-    T &operator[](int);
+    T &operator[](int); //0,1,..... size-1
     void sort();
     bool is_sorted();
     void reverse();
@@ -33,38 +33,63 @@ private:
     int prev(int);
 };
 
+//Constructor por defecto 
 template <class T>
-CircularArray<T>::CircularArray()
-{
-    CircularArray(0);
+CircularArray<T>::CircularArray() {
+    this->capacity = 0;
+    this->front = this->back = -1;
+    this->arr = new T[0];
+    this->len = 0;
 }
 
+
+//constructor que recibe la capacidad inicial
 template <class T>
 CircularArray<T>::CircularArray(int _capacity)
 {
     this->array = new T[_capacity];
     this->capacity = _capacity;
-    this->front = this->back = -1;
+    this->front = this->back = -1; //Tanto el back como el front -1, inidica que no hay elementos
+    this->len = 0;
 }
 
+//Liberamos el array------------------------
 template <class T>
 CircularArray<T>::~CircularArray()
 {
+    //delete[] this->array;
     delete[] array;
 }
+//-------------------------------------------
+
 
 template <class T>
-int CircularArray<T>::prev(int index)
+int CircularArray<T>::prev(int index) //Obtiene la posicion anterior del index,front
 {
+                    // ?      1 true   :   0 false
     return (index == 0) ? capacity - 1 : index - 1;
 }
 
 template <class T>
-int CircularArray<T>::next(int index)
+int CircularArray<T>::next(int index)//Obtiene la siguiente posicion, back
 {
     return (index + 1) % capacity;
+    //return (index == capacity - 1)? 0 : index + 1;
 }
 
+template <class T>
+void CircularArray<T>::push_back(T data){
+    
+    if(is_full()) resize();
+    if(is_empty())
+        this->back = this->front = 0;
+    else
+        this->back = next(this->back);
+    array[this->back]=data; //insertando en la siguiente posición
+
+}
+
+//Recorriendo 
 template <class T>
 string CircularArray<T>::to_string(string sep)
 {
@@ -72,4 +97,9 @@ string CircularArray<T>::to_string(string sep)
     for (int i = 0; i < size(); i++)
         result += std::to_string((*this)[i]) + sep;
     return result;    
+}
+
+
+int main(){
+    
 }
--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 int main() {
-    CircularArray<int>* array = new CircularArray<int>(7);
+    CircularArray<int>* array = new CircularArray<int>(7); // Array capacidad 7
     ASSERT(array->is_empty() == true, "The function is_empty is not working");
+    //Insertando elementos
     array->push_back(6);
     array->push_back(3);
     array->push_back(7);
@@ -14,6 +15,7 @@ int main() {
     array->push_back(1);
     ASSERT(array->is_empty() == false, "The function is_empty is not working");
     ASSERT(array->is_full() == false, "The function is_full is not working");
+
     array->pop_front();
     array->pop_back();
     array->push_front(10);
@@ -21,12 +23,19 @@ int main() {
     array->push_front(15);
     array->push_front(4);
     ASSERT(array->to_string() == "4 15 2 10 3 7 8 ", "The function push_front is not working");
+    // ASSERT((*array)[1] == 15, "The operator [] push_front is not working");
+    // ASSERT((*array)[7] == 7, "The operator [] push_front is not working");
     ASSERT(array->is_full() == true, "The function is_full is not working");
     ASSERT(array->is_sorted() == false, "The function is_sorted is not working");
     array->sort();
     ASSERT(array->to_string() == "2 3 4 7 8 10 15 ", "The function sort is not working");
     array->reverse();
     ASSERT(array->to_string() == "15 10 8 7 4 3 2 ", "The function reverse is not working");   
+    
+    for(int i=0;i<array->size();i++)
+        cout<<(*array)[i]<<endl;
+    
+
     delete array;
     return 0;
 }
